7a3e5acc4d551e4ee4488aab6c44dc2d5df67f15
ShenJianchun <shenjianchun@gmail.com>
2016-06-26 17:09:03 +0800

    添加静态内部类实现的单例模式

---
+++ src/com/github/pattern/Singleton/Singleton2.java
@@ -0,0 +1,21 @@
+package com.github.pattern.Singleton;
+
+/**
+ * 使用静态内部类单例模式
+ * Created by JC on 2016-06-26.
+ */
+public class Singleton2 {
+
+    private Singleton2(){}
+
+    public static Singleton2 getInstance() {
+        return SingletonHolder.sInstance;
+    }
+
+    /**
+     * 静态内部类
+     */
+    private static class SingletonHolder {
+        private static final Singleton2 sInstance = new Singleton2();
+    }
+}
