dea824d4a47a73e3aca4aacb7562aa2d38d59087
ShenJianchun <shenjianchun@gmail.com>
2016-06-26 16:55:18 +0800

    初始化

---
+++ src/com/github/pattern/Singleton.java
@@ -0,0 +1,28 @@
+package com.github.pattern;
+
+/**
+ * 使用Double Check Lock 实现单例模式
+ * Created by JC on 2016-06-26.
+ */
+public class Singleton {
+
+    private static Singleton sInstance = null;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getsInstance() {
+
+        if (sInstance == null) {
+            synchronized (Singleton.class) {
+                if (sInstance == null) {
+                    sInstance = new Singleton();
+                }
+            }
+        }
+
+        return sInstance;
+    }
+
+}
